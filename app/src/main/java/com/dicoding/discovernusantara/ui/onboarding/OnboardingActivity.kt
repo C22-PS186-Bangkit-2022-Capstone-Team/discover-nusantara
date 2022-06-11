27c68d9207a4de5342601b41aedd8e31322f0549
hueyanroberto <hueyan.roberto@gmail.com>
2022-06-12 03:08:19 +0700

    on boarding starter

---
+++ app/src/main/java/com/dicoding/discovernusantara/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,30 @@
+package com.dicoding.discovernusantara.ui.onboarding
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.dicoding.discovernusantara.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
